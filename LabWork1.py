1f5911e406ba8992475c7eecf4a9f0536fcccbef
Mugen203 <kwakuaffram@gmail.com>
2019-07-16 17:54:09 +0000

    Day2

---
+++ LabWork1.py
@@ -0,0 +1,23 @@
+i = "Labwork"
+print(i)
+
+ii = "Labworks'"
+print(ii)
+
+iii = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\n Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+print(iii)
+
+iv = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+print(iv)
+
+v = "LabWork"
+print(len(v))
+
+vi = "Labwork" + "Experiment"
+print(vi)
+
+vii = "Labwork, Experiment"
+print(vii.split(","))
+
+viii = "Bazinga"
+print(viii[2:6])
