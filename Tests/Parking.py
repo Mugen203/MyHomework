b64b462aa6f179ffdfca3c934b65b74d1ea50f4f
Mugen203 <kwakuaffram@gmail.com>
2019-07-31 17:04:44 +0000

    Account class with Owuraku's advice

---
+++ Tests/Parking.py
@@ -0,0 +1,10 @@
+#Beginning of the program
+#Class for the parking fee.
+class ParkingFee(object):
+
+    def __init__(self, unrestricted, handicapped):
+        self.unrestricted = unrestricted 
+        self.handicapped = handicapped
+
+    pass
+
