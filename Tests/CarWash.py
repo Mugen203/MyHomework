97d135c0e40e13ace6e00989ede6a724dae85560
Mugen203 <kwakuaffram@gmail.com>
2019-08-05 18:34:09 +0000

    Done with sms structure

--- Tests/CarWash.py
+++ Tests/CarWash.py
@@ -1,18 +1,22 @@
 import time
 import datetime
+from config import client
 
 #Class to hold customer info
+
 class Customer:
     print("\nWelcome Customer!\n")
 
+    # Had to comment out the constructor because it was not needed
+
     # def __init__ (self, name, license_no, car_type, car_brand, phone_no, email, twitter):
-    #     self.name = name
-    #     self.license_no = license_no
-    #     self.car_brand = car_brand
-    #     self.car_type = car_type
-    #     self.phone_no =  phone_no
-    #     self.email = email
-    #     self.twitter = twitter
+    #      self.name = name
+    #      self.license_no = license_no
+    #      self.car_brand = car_brand
+    #      self.car_type = car_type
+    #      self.phone_no =  phone_no
+    #      self.email = email
+    #      self.twitter = twitter
 
     def getInfo(self):
         global customer_info
@@ -26,7 +30,11 @@ class Customer:
         self.twitter = str(input("Please enter your twitter handle: "))
 
         customer_info = [self.name, self.license_no, self.car_brand, self.car_type, self.phone_no, self.email, self.twitter]
-        #print(customer_info)
+
+
+        #print(customer_info) - Was used to test if the variable customer_info stored the info
+
+        
         return customer_info
 
         
@@ -36,16 +44,13 @@ class Login(Customer):
 
 
 
-
-
-
-
-
-
-
-
-
-
+class SMS(Customer):
+    def send_message(self):
+        client.messages.create(
+            body= "You will be notified as soon as your car is ready. Thank you for using our service!",
+            from_="7753641017",
+            to = customer_info[4]
+        )
 
 
 
@@ -53,3 +58,6 @@ class Login(Customer):
 
 Customer1 = Customer()
 Customer1.getInfo()
+
+client1 = SMS()
+client1.send_message()
--- Tests/SMS.py
+++ Tests/SMS.py
@@ -1,10 +1,13 @@
 from config import client
 import CarWash
 
-class SMS:
-    def sms():
-        
+class SMS(Customer):
+    def send_message(self):
+        client_no = customer_info[4]
+        for number in client_no:
+            client.messages.create(
+                body= "You will be notified as soon as your car is ready. Thank you for using our service!",
+                from_="7753641017",
+                to = "client_no"
+        )
 
-client.messages.create(
-     to=""
- )
